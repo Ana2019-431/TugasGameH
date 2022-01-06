df5ba765f34efd6b09554b55f23643f62f5fe5b5
Ana <79823071+Ana2019-431@users.noreply.github.com>
2022-01-06 22:32:30 +0700

    Add files via upload

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+// AUTHOR : CAPTAIN MOCH RAFFLI MAULANA SIR!!! 
+// NIM : 201910370311431
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =player.transform.position + offset;
+    }
+}
---
+++ Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+// AUTHOR : CAPTAIN MOCH RAFFLI MAULANA SIR!!! 
+// NIM : 201910370311431
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the game object that this script is attached to by 15 in the X axis,
+        // 30 in Y axis and 45 in the Z axis, multiplied by deltaTime in order to make it per second
+        // rather than per frame.
+        transform.Rotate (new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
